a5c5639e466d1efcf09cda63fef9115ecf813329
Silas Silva <silas@github.com>
2023-05-15 18:24:05 -0300

    feito

--- app/page.js
+++ app/page.js
@@ -1,95 +1,77 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import React from 'react';
+import { useState } from 'react';
+
+
+//Coloque o código dos demais componentes aqui...
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    
+  const [blogMessages, setBlogMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [ispesquisa, setIspesquisa] = useState(false);
+  
+  const handleSearch = event => {
+    const term = event.target.value.toLowerCase();
+    if (term === ''){
+      setIspesquisa(false)
+    }else{
+      setIspesquisa(true)
+    }
+    setSearchTerm(term);
+    const filteredMessages = blogMessages.filter(
+      message =>
+        message[1].toString().toLowerCase().includes(term) || // Verifica se o termo está presente no autor
+        message[0].toString().toLowerCase().includes(term) // Verifica se o termo está presente na mensagem
+    );
+    setMessages(filteredMessages);
+  };
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  fetch('https://script.google.com/macros/s/AKfycbzBn3sALe1rYjz7Ze-Ik7q9TEVP0I2V3XX7GNcecWP8NvCzGt4yO_RT1OlQp09TE9cU/exec')
+    .then(response => response.json())
+    .then(data => {
+        setBlogMessages(data);
+    });
+    return (
+      <main className={styles.main}>
+        <form>
+          <label>Procure uma mensagem:</label>
+          <input style={{width : '100%'}} type="text" placeholder="Search..." value={searchTerm} onChange={handleSearch}/>
+        </form>
+        
+        <table>
+          <thead>
+            <tr>
+              <th>Author</th>
+              <th>Message</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            ispesquisa
+                ? messages.map(message => (
+                  <tr key={message.id}>
+                    <td>{message[1]}</td>
+                    <td>{message[0]}</td>
+                    <td>{message[2]}</td>
+                  </tr>
+                ))
+              :
+                blogMessages.map(message => (
+                  <tr key={message.id}>
+                    <td>{message[1]}</td>
+                    <td>{message[0]}</td>
+                    <td>{message[2]}</td>
+                  </tr>
+                ))
+            }
+          </tbody>
+        </table>
+      </main>
+    )
 }
